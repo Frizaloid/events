e7a5e69cd60d80e380a86d6b33768ed7195dda6f
Frizaloid <68231567+Frizaloid@users.noreply.github.com>
2020-07-13 17:51:28 +0500

    Create practice_script.py
    Задание №3: скрипт для прослушивания ивентов с вебсокет сервера.

---
+++ practice_script.py
@@ -0,0 +1,28 @@
+import websockets
+import requests
+import asyncio
+
+async def newEvent():
+    uri = 'ws://miac74.ru:9998'
+    async with websockets.connect(uri) as websocket:
+        eventCount = 0
+        while True:
+            if eventCount < 201:
+                f = open("events.txt", "a")
+                event = await websocket.recv()
+                f.write(event + '\n')
+                f.close()
+                eventCount += 1
+            else:
+                eventCount = 0
+                f = open("events.txt", "r")
+                for line in f:
+                    data = line
+                    requests.post("Тут нужен URL", data = data)
+                f.close()
+                f.open("events.txt", "w")
+                f.write("")
+                f.close()
+
+asyncio.get_event_loop().run_until_complete(newEvent())
+asyncio.get_event_loop().run_forever()
